7f2e47697903e43eecf3fc99dfed409be4ba5632
0xchain1 <0xchain1@protonmail.com>
2021-09-04 00:12:18 -0400

    deploying amm

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,43 @@
 
+import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-web3";
+import "@nomiclabs/hardhat-etherscan";
+require('dotenv').config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// // https://hardhat.org/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async (args, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(await account.address);
-  }
-});
-
+//   for (const account of accounts) {
+//     console.log(await account.address);
+//   }
+// });
 
 export default {
-  solidity: "0.7.3",
-};
+  solidity: {
+    version: "0.6.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    kovan: {
+        gas: "auto",
+        gasPrice: "auto",
+        url: process.env.API_KEY,
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    matic: {
+      url: process.env.MATIC_API_KEY,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+  },
+  etherscan: {
+  	apiKey: process.env.POLYGONSCAN_API_KEY
+  }
+}
---
+++ scripts/deploy.ts
@@ -0,0 +1,31 @@
+import { run, ethers } from "hardhat";
+
+async function main() {
+  await run("compile");
+
+  const [deployer] = await ethers.getSigners();
+  console.log(`Deploying contracts with ${deployer.address}`);
+
+  const balance = await deployer.getBalance();
+  console.log(`Account balance: ${balance.toString()}`)
+
+  // wrapped matic address
+  const WETH_ADDRESS = "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270";
+
+  const FactoryContract = await ethers.getContractFactory("UniswapV2Factory")
+
+
+  const factory = await FactoryContract.deploy(deployer.address)
+  console.log(`Factory address: ${factory.address}`)
+
+  const RouterContract = await ethers.getContractFactory("UniswapV2Router02")
+  const router = await RouterContract.deploy(factory.address,WETH_ADDRESS)
+  console.log(`Router address: ${router.address}`)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
