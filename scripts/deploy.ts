e06d7303e616bf298ec181ecf12ce37386c9f811
0xchain1 <0xchain1@protonmail.com>
2021-09-04 00:11:11 -0400

    deploy working

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -10,18 +10,18 @@ async function main() {
   console.log(`Account balance: ${balance.toString()}`)
 
   // wrapped matic address
-  const WETH_ADDRESS = 0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270;
+  const WETH_ADDRESS = "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270";
 
-  // const FactoryContract = await ethers.getContractFactory("UniswapV2Factory")
-  console.log(deployer.address)
+  const FactoryContract = await ethers.getContractFactory("UniswapV2Factory")
 
-//   const factory = await FactoryContract.deploy(deployer.address)
-//   console.log(`Facotry address: ${factory.address}`)
 
-//   const RouterContract = await ethers.getContractFactory("UniswapV2Router02")
-//   const router = await RouterContract.deploy(factory.address,WETH_ADDRESS)
-//   console.log(`Router address: ${router.address}`)
-// }
+  const factory = await FactoryContract.deploy(deployer.address)
+  console.log(`Factory address: ${factory.address}`)
+
+  const RouterContract = await ethers.getContractFactory("UniswapV2Router02")
+  const router = await RouterContract.deploy(factory.address,WETH_ADDRESS)
+  console.log(`Router address: ${router.address}`)
+}
 
 main()
   .then(() => process.exit(0))
