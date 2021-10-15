53148740d8327557505fe63d702d52e042ae2d92
Baboo Bhaiya <baboobhaiya@protonmail.com>
2021-10-15 15:35:06 +0100

    added masterchef contracts + tests

---
+++ contracts/rewards/README.md
@@ -0,0 +1,5 @@
+## Notes:
+
+Using the rewards contracts from SushiSwap
+### Masterchef
+1. Have removed migrator and devaddr functions/variables
---
+++ test/time.ts
@@ -0,0 +1,11 @@
+const { ethers } = require("hardhat")
+
+export async function advanceBlock() {
+  return ethers.provider.send("evm_mine", [])
+}
+
+export async function advanceBlockTo(blockNumber: any) {
+  for (let i = await ethers.provider.getBlockNumber(); i < blockNumber; i++) {
+    await advanceBlock()
+  }
+}
