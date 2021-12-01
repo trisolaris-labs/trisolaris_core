16c39e4eeba1f545bce98b9cbe9aba9212a1e551
Baboo Bhaiya <baboobhaiya@protonmail.com>
2021-12-01 14:26:27 +0000

    passing trimaker tests

--- test/rewards/trimaker.test.ts
+++ test/rewards/trimaker.test.ts
@@ -1,33 +1,54 @@
 import { expect } from "chai";
-import { prepare, deploy, getBigNumber, createSLP } from "./utilities"
+import { ethers } from "hardhat";
+import { getBigNumber, createSLP } from "../utils"
 
 describe("TriMaker", function () {
   before(async function () {
-    await prepare(this, ["TriMaker", "TriBar", "TriMakerExploitMock", "ERC20Mock", "UniswapV2Factory", "UniswapV2Pair"])
+    this.signers = await ethers.getSigners()
+    this.alice = this.signers[0]
+    this.bob = this.signers[1]
+    this.carol = this.signers[2]
+    this.minter = this.signers[4]
+
+    this.TriMaker = await ethers.getContractFactory("TriMaker")
+    this.TriBar = await ethers.getContractFactory("TriBar")
+    this.ERC20Mock = await ethers.getContractFactory("ERC20Mock", this.minter)
+    this.UniswapV2Factory = await ethers.getContractFactory("UniswapV2Factory")
+    this.UniswapV2Pair = await ethers.getContractFactory("UniswapV2Pair")
+    this.ZeroAddress = "0x0000000000000000000000000000000000000000"
   })
 
   beforeEach(async function () {
-    await deploy(this, [
-      ["tri", this.ERC20Mock, ["TRI", "TRI", getBigNumber("10000000")]],
-      ["dai", this.ERC20Mock, ["DAI", "DAI", getBigNumber("10000000")]],
-      ["mic", this.ERC20Mock, ["MIC", "MIC", getBigNumber("10000000")]],
-      ["usdc", this.ERC20Mock, ["USDC", "USDC", getBigNumber("10000000")]],
-      ["weth", this.ERC20Mock, ["WETH", "ETH", getBigNumber("10000000")]],
-      ["strudel", this.ERC20Mock, ["$TRDL", "$TRDL", getBigNumber("10000000")]],
-      ["factory", this.UniswapV2Factory, [this.alice.address]],
-    ])
-    await deploy(this, [["bar", this.TriBar, [this.tri.address]]])
-    await deploy(this, [["triMaker", this.TriMaker, [this.factory.address, this.bar.address, this.tri.address, this.weth.address]]])
-    await deploy(this, [["exploiter", this.TriMakerExploitMock, [this.triMaker.address]]])
-    await createSLP(this, "triEth", this.tri, this.weth, getBigNumber(10))
-    await createSLP(this, "strudelEth", this.strudel, this.weth, getBigNumber(10))
-    await createSLP(this, "daiEth", this.dai, this.weth, getBigNumber(10))
-    await createSLP(this, "usdcEth", this.usdc, this.weth, getBigNumber(10))
-    await createSLP(this, "micUSDC", this.mic, this.usdc, getBigNumber(10))
-    await createSLP(this, "triUSDC", this.tri, this.usdc, getBigNumber(10))
-    await createSLP(this, "daiUSDC", this.dai, this.usdc, getBigNumber(10))
-    await createSLP(this, "daiMIC", this.dai, this.mic, getBigNumber(10))
+    this.tri = await this.ERC20Mock.connect(this.minter).deploy("TRI", "TRI", getBigNumber("10000000"))
+    await this.tri.deployed()
+    this.dai = await this.ERC20Mock.connect(this.minter).deploy("DAI", "DAI", getBigNumber("10000000"))
+    await this.dai.deployed()
+    this.mic = await this.ERC20Mock.connect(this.minter).deploy("MIC", "MIC", getBigNumber("10000000"))
+    await this.mic.deployed()
+    this.usdc = await this.ERC20Mock.connect(this.minter).deploy("USDC", "USDC", getBigNumber("10000000"))
+    await this.usdc.deployed()
+    this.weth = await this.ERC20Mock.connect(this.minter).deploy("WETH", "ETH", getBigNumber("10000000"))
+    await this.usdc.deployed()
+    this.strudel = await this.ERC20Mock.connect(this.minter).deploy("$TRDL", "$TRDL", getBigNumber("10000000"))
+    await this.strudel.deployed()
+    this.factory = await this.UniswapV2Factory.connect(this.minter).deploy(this.minter.address)
+    await this.factory.deployed()
+
+    this.bar = await this.TriBar.connect(this.minter).deploy(this.tri.address)
+    await this.bar.deployed()
+    this.triMaker = await this.TriMaker.connect(this.minter).deploy(this.factory.address, this.bar.address, this.tri.address, this.weth.address)
+    await this.triMaker.deployed()
+
+    await createSLP(this, "triEth", this.tri, this.weth, getBigNumber(10), this.minter)
+    await createSLP(this, "strudelEth", this.strudel, this.weth, getBigNumber(10), this.minter)
+    await createSLP(this, "daiEth", this.dai, this.weth, getBigNumber(10), this.minter)
+    await createSLP(this, "usdcEth", this.usdc, this.weth, getBigNumber(10), this.minter)
+    await createSLP(this, "micUSDC", this.mic, this.usdc, getBigNumber(10), this.minter)
+    await createSLP(this, "triUSDC", this.tri, this.usdc, getBigNumber(10), this.minter)
+    await createSLP(this, "daiUSDC", this.dai, this.usdc, getBigNumber(10), this.minter)
+    await createSLP(this, "daiMIC", this.dai, this.mic, getBigNumber(10), this.minter)
   })
+
   describe("setBridge", function () {
     it("does not allow to set bridge for Tri", async function () {
       await expect(this.triMaker.setBridge(this.tri.address, this.weth.address)).to.be.revertedWith("TriMaker: Invalid bridge")
@@ -41,6 +62,10 @@ describe("TriMaker", function () {
       await expect(this.triMaker.setBridge(this.dai.address, this.dai.address)).to.be.revertedWith("TriMaker: Invalid bridge")
     })
 
+    it("does not allow to non_owner to set bridge", async function () {
+      await expect(this.triMaker.connect(this.alice).setBridge(this.mic.address, this.dai.address)).to.be.revertedWith("Ownable: caller is not the owner")
+    })
+
     it("emits correct event on bridge", async function () {
       await expect(this.triMaker.setBridge(this.dai.address, this.tri.address))
         .to.emit(this.triMaker, "LogBridgeSet")
@@ -49,15 +74,15 @@ describe("TriMaker", function () {
   })
   describe("convert", function () {
     it("should convert TRI - ETH", async function () {
-      await this.triEth.transfer(this.triMaker.address, getBigNumber(1))
-      await this.triMaker.convert(this.tri.address, this.weth.address)
+      await this.triEth.connect(this.minter).transfer(this.triMaker.address, getBigNumber(1))
+      await this.triMaker.connect(this.alice).convert(this.tri.address, this.weth.address)
       expect(await this.tri.balanceOf(this.triMaker.address)).to.equal(0)
       expect(await this.triEth.balanceOf(this.triMaker.address)).to.equal(0)
       expect(await this.tri.balanceOf(this.bar.address)).to.equal("1897569270781234370")
     })
 
     it("should convert USDC - ETH", async function () {
-      await this.usdcEth.transfer(this.triMaker.address, getBigNumber(1))
+      await this.usdcEth.connect(this.minter).transfer(this.triMaker.address, getBigNumber(1))
       await this.triMaker.convert(this.usdc.address, this.weth.address)
       expect(await this.tri.balanceOf(this.triMaker.address)).to.equal(0)
       expect(await this.usdcEth.balanceOf(this.triMaker.address)).to.equal(0)
@@ -65,7 +90,7 @@ describe("TriMaker", function () {
     })
 
     it("should convert $TRDL - ETH", async function () {
-      await this.strudelEth.transfer(this.triMaker.address, getBigNumber(1))
+      await this.strudelEth.connect(this.minter).transfer(this.triMaker.address, getBigNumber(1))
       await this.triMaker.convert(this.strudel.address, this.weth.address)
       expect(await this.tri.balanceOf(this.triMaker.address)).to.equal(0)
       expect(await this.strudelEth.balanceOf(this.triMaker.address)).to.equal(0)
@@ -73,7 +98,7 @@ describe("TriMaker", function () {
     })
 
     it("should convert USDC - TRI", async function () {
-      await this.triUSDC.transfer(this.triMaker.address, getBigNumber(1))
+      await this.triUSDC.connect(this.minter).transfer(this.triMaker.address, getBigNumber(1))
       await this.triMaker.convert(this.usdc.address, this.tri.address)
       expect(await this.tri.balanceOf(this.triMaker.address)).to.equal(0)
       expect(await this.triUSDC.balanceOf(this.triMaker.address)).to.equal(0)
@@ -81,7 +106,7 @@ describe("TriMaker", function () {
     })
 
     it("should convert using standard ETH path", async function () {
-      await this.daiEth.transfer(this.triMaker.address, getBigNumber(1))
+      await this.daiEth.connect(this.minter).transfer(this.triMaker.address, getBigNumber(1))
       await this.triMaker.convert(this.dai.address, this.weth.address)
       expect(await this.tri.balanceOf(this.triMaker.address)).to.equal(0)
       expect(await this.daiEth.balanceOf(this.triMaker.address)).to.equal(0)
@@ -89,7 +114,7 @@ describe("TriMaker", function () {
     })
 
     it("converts MIC/USDC using more complex path", async function () {
-      await this.micUSDC.transfer(this.triMaker.address, getBigNumber(1))
+      await this.micUSDC.connect(this.minter).transfer(this.triMaker.address, getBigNumber(1))
       await this.triMaker.setBridge(this.usdc.address, this.tri.address)
       await this.triMaker.setBridge(this.mic.address, this.usdc.address)
       await this.triMaker.convert(this.mic.address, this.usdc.address)
@@ -99,7 +124,7 @@ describe("TriMaker", function () {
     })
 
     it("converts DAI/USDC using more complex path", async function () {
-      await this.daiUSDC.transfer(this.triMaker.address, getBigNumber(1))
+      await this.daiUSDC.connect(this.minter).transfer(this.triMaker.address, getBigNumber(1))
       await this.triMaker.setBridge(this.usdc.address, this.tri.address)
       await this.triMaker.setBridge(this.dai.address, this.usdc.address)
       await this.triMaker.convert(this.dai.address, this.usdc.address)
@@ -109,7 +134,7 @@ describe("TriMaker", function () {
     })
 
     it("converts DAI/MIC using two step path", async function () {
-      await this.daiMIC.transfer(this.triMaker.address, getBigNumber(1))
+      await this.daiMIC.connect(this.minter).transfer(this.triMaker.address, getBigNumber(1))
       await this.triMaker.setBridge(this.dai.address, this.usdc.address)
       await this.triMaker.setBridge(this.mic.address, this.dai.address)
       await this.triMaker.convert(this.dai.address, this.mic.address)
@@ -119,12 +144,13 @@ describe("TriMaker", function () {
     })
 
     it("reverts if it loops back", async function () {
-      await this.daiMIC.transfer(this.triMaker.address, getBigNumber(1))
+      await this.daiMIC.connect(this.minter).transfer(this.triMaker.address, getBigNumber(1))
       await this.triMaker.setBridge(this.dai.address, this.mic.address)
       await this.triMaker.setBridge(this.mic.address, this.dai.address)
       await expect(this.triMaker.convert(this.dai.address, this.mic.address)).to.be.reverted
     })
 
+    /*
     it("reverts if caller is not EOA", async function () {
       await this.triEth.transfer(this.triMaker.address, getBigNumber(1))
       await expect(this.exploiter.convert(this.tri.address, this.weth.address)).to.be.revertedWith("TriMaker: must use EOA")
@@ -133,9 +159,10 @@ describe("TriMaker", function () {
     it("reverts if pair does not exist", async function () {
       await expect(this.triMaker.convert(this.mic.address, this.micUSDC.address)).to.be.revertedWith("TriMaker: Invalid pair")
     })
+    */
 
     it("reverts if no path is available", async function () {
-      await this.micUSDC.transfer(this.triMaker.address, getBigNumber(1))
+      await this.micUSDC.connect(this.minter).transfer(this.triMaker.address, getBigNumber(1))
       await expect(this.triMaker.convert(this.mic.address, this.usdc.address)).to.be.revertedWith("TriMaker: Cannot convert")
       expect(await this.tri.balanceOf(this.triMaker.address)).to.equal(0)
       expect(await this.micUSDC.balanceOf(this.triMaker.address)).to.equal(getBigNumber(1))
@@ -145,8 +172,8 @@ describe("TriMaker", function () {
 
   describe("convertMultiple", function () {
     it("should allow to convert multiple", async function () {
-      await this.daiEth.transfer(this.triMaker.address, getBigNumber(1))
-      await this.triEth.transfer(this.triMaker.address, getBigNumber(1))
+      await this.daiEth.connect(this.minter).transfer(this.triMaker.address, getBigNumber(1))
+      await this.triEth.connect(this.minter).transfer(this.triMaker.address, getBigNumber(1))
       await this.triMaker.convertMultiple([this.dai.address, this.tri.address], [this.weth.address, this.weth.address])
       expect(await this.tri.balanceOf(this.triMaker.address)).to.equal(0)
       expect(await this.daiEth.balanceOf(this.triMaker.address)).to.equal(0)
