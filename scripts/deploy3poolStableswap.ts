4013af9b504d298ad71779f909de0fd097fdc40b
Eric Juta <ericjohnjuta@gmail.com>
2022-05-11 19:42:03 +0100

    feat(scripts/deploy3poolstableswap.ts): verify scripts

--- scripts/deploy3poolStableswap.ts
+++ scripts/deploy3poolStableswap.ts
@@ -2,7 +2,10 @@
 // but useful for running the script in a standalone fashion through `node <script>`.
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers, run } from "hardhat";
+import {
+  ethers,
+  // , run
+} from "hardhat";
 
 async function main(): Promise<void> {
   // Hardhat always runs the compile task when running scripts through it.
@@ -82,28 +85,38 @@ async function main(): Promise<void> {
   const lpToken = LpTokenFactory.attach(swapStorage.lpToken);
   console.log(`lpToken deployed at ${lpToken.address}`);
 
-  // Verify contracts
-  await run("verify:verify", {
-    address: lpTokenBase.address,
-    constructorArguments: [],
-  });
-
-  await run("verify:verify", {
-    address: amplificationUtils.address,
-    constructorArguments: [],
-  });
-
-  await run("verify:verify", {
-    address: swapFlashLoan.address,
-    constructorArguments: [],
-  });
-
-  //   Deploying contracts with 0x620b5A0998e19B47681A02055bfcD0B856AbC375
+  // Deploying contracts with 0x620b5A0998e19B47681A02055bfcD0B856AbC375
   // LPToken Base deployed at 0xB77190A4fD2528d2Bb778B409FB5224f7ffaCB24
   // amplificationUtils deployed at 0x114ECaa70256aFAd393f733aA4B4bF61c8959fc2
   // swapUtils deployed at 0x0564d68404608599e8c567A0bD74F90a942A69A0
   // swapFlashLoan deployed at 0x458459E48dbAC0C8Ca83F8D0b7b29FEfE60c3970
   // lpToken deployed at 0x87BCC091d0A7F9352728100268Ac8D25729113bB
+
+  // Verify contracts
+  // await run("verify:verify", {
+  //   address: "0xB77190A4fD2528d2Bb778B409FB5224f7ffaCB24",
+  //   constructorArguments: [],
+  // });
+
+  // await run("verify:verify", {
+  //   address: "0x114ECaa70256aFAd393f733aA4B4bF61c8959fc2",
+  //   constructorArguments: [],
+  // });
+
+  // await run("verify:verify", {
+  //   address: "0x0564d68404608599e8c567A0bD74F90a942A69A0",
+  //   constructorArguments: [],
+  // });
+
+  // await run("verify:verify", {
+  //   address: "0x458459E48dbAC0C8Ca83F8D0b7b29FEfE60c3970",
+  //   constructorArguments: [],
+  // });
+
+  // await run("verify:verify", {
+  //   address: "0x87BCC091d0A7F9352728100268Ac8D25729113bB",
+  //   constructorArguments: [],
+  // });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
