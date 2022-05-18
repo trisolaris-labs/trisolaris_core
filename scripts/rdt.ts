8cf5b0cce5d45eb22ef2776dfeabcc3b6dc6c9fe
specialist0x <specialist0x@protonmail.com>
2022-05-18 19:13:18 -0400

    updated rdt script with deploy args

--- scripts/rdt.ts
+++ scripts/rdt.ts
@@ -2,12 +2,11 @@
 // but useful for running the script in a standalone fashion through `node <script>`.
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import {
-  RevenueDistributionToken__factory,
-} from "../typechain";
-import { ethers } from "hardhat";
+import { RevenueDistributionToken__factory } from "../typechain";
+import { ethers, run } from "hardhat";
 
 const USDT_ADDRESS = "0x4988a896b1227218e4A686fdE5EabdcAbd91571f";
+const TRI_ADDRESS = "0xfa94348467f64d5a457f75f8bc40495d33c65abb";
 
 async function main(): Promise<void> {
   // Hardhat always runs the compile task when running scripts through it.
@@ -23,10 +22,15 @@ async function main(): Promise<void> {
   console.log(`Account balance: ${balance.toString()}`);
 
   const rdtFactory: RevenueDistributionToken__factory = await ethers.getContractFactory("RevenueDistributionToken");
-  const factory = await rdtFactory.deploy("Test xTRI V2", "xTRI", deployer.address, USDT_ADDRESS, 6);
+  const factory = await rdtFactory.deploy("Test xTRI V2", "xTRI", deployer.address, USDT_ADDRESS, "100", TRI_ADDRESS);
   await factory.deployed();
-  
+
   console.log(`RDT address: ${factory.address}`);
+
+  await run("verify:verify", {
+    address: factory.address,
+    constructorArguments: ["Test xTRI V2", "xTRI", deployer.address, USDT_ADDRESS, "100", TRI_ADDRESS],
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
