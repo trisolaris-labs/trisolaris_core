f07d7642e724c53e4a24cf83ab94a19228b1238f
Eric Juta <ericjohnjuta@gmail.com>
2022-05-18 14:27:20 -0400

    feat(test/revenuedistributiontoken/migrate.test.ts): add migrate()

--- contracts/rdt/RevenueDistributionToken.sol
+++ contracts/rdt/RevenueDistributionToken.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.7;
 
-import { ERC20 }       from "./ERC20.sol";
+import { ERC20 } from "./ERC20.sol";
 import { ERC20Helper } from "./ERC20Helper.sol";
+import { ITriBar } from "../rewards/interfaces/ITriBar.sol";
 
 import { IRevenueDistributionToken } from "./interfaces/IRevenueDistributionToken.sol";
 
@@ -16,21 +17,20 @@ import { IRevenueDistributionToken } from "./interfaces/IRevenueDistributionToke
 */
 
 contract RevenueDistributionToken is IRevenueDistributionToken, ERC20 {
+    uint256 public immutable override precision; // Precision of rates, equals max deposit amounts before rounding errors occur
 
-    uint256 public immutable override precision;  // Precision of rates, equals max deposit amounts before rounding errors occur
-
-    address public override asset;                // Underlying revenue share asset
+    address public override asset; // Underlying revenue share asset
     address public revenueAsset;
 
-    address public override owner;                // Current owner of the contract, able to update the vesting schedule.
-    address public override pendingOwner;         // Pending owner of the contract, able to accept ownership.
+    address public override owner; // Current owner of the contract, able to update the vesting schedule.
+    address public override pendingOwner; // Pending owner of the contract, able to accept ownership.
 
-    uint256 public override freeAssets;           // Amount of revenue assets unlocked regardless of time passed.
-    uint256 public override issuanceRate;         // asset/second rate dependent on aggregate vesting schedule.
-    uint256 public override lastUpdated;          // Timestamp of when issuance equation was last updated.
-    uint256 public override vestingPeriodFinish;  // Timestamp when current vesting schedule ends.
+    uint256 public override freeAssets; // Amount of revenue assets unlocked regardless of time passed.
+    uint256 public override issuanceRate; // asset/second rate dependent on aggregate vesting schedule.
+    uint256 public override lastUpdated; // Timestamp of when issuance equation was last updated.
+    uint256 public override vestingPeriodFinish; // Timestamp when current vesting schedule ends.
 
-    uint256 private locked = 1;  // Used in reentrancy check.
+    uint256 private locked = 1; // Used in reentrancy check.
 
     /*****************/
     /*** Modifiers ***/
@@ -46,14 +46,25 @@ contract RevenueDistributionToken is IRevenueDistributionToken, ERC20 {
         locked = 1;
     }
 
-    constructor(string memory name_, string memory symbol_, address owner_, address revenueAsset_, uint256 precision_, address asset_)
-        ERC20(name_, symbol_, ERC20(revenueAsset_).decimals())
-    {
+    /*****************/
+    /*** Events ***/
+    /*****************/
+
+    event Migrated(address xTRI, address asset, uint256 triUnstaked, uint256 shares);
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        address owner_,
+        address revenueAsset_,
+        uint256 precision_,
+        address asset_
+    ) ERC20(name_, symbol_, ERC20(revenueAsset_).decimals()) {
         require((owner = owner_) != address(0), "RDT:C:OWNER_ZERO_ADDRESS");
 
-        revenueAsset     = revenueAsset_;  // Don't need to check zero address as ERC20(asset_).decimals() will fail in ERC20 constructor.
-        precision        = precision_;
-        asset            = asset_;  
+        revenueAsset = revenueAsset_; // Don't need to check zero address as ERC20(asset_).decimals() will fail in ERC20 constructor.
+        precision = precision_;
+        asset = asset_;
     }
 
     /********************************/
@@ -65,7 +76,7 @@ contract RevenueDistributionToken is IRevenueDistributionToken, ERC20 {
 
         emit OwnershipAccepted(owner, msg.sender);
 
-        owner        = msg.sender;
+        owner = msg.sender;
         pendingOwner = address(0);
     }
 
@@ -77,15 +88,22 @@ contract RevenueDistributionToken is IRevenueDistributionToken, ERC20 {
         emit PendingOwnerSet(msg.sender, pendingOwner_);
     }
 
-    function updateVestingSchedule(uint256 vestingPeriod_) external virtual override returns (uint256 issuanceRate_, uint256 freeAssets_) {
+    function updateVestingSchedule(uint256 vestingPeriod_)
+        external
+        virtual
+        override
+        returns (uint256 issuanceRate_, uint256 freeAssets_)
+    {
         require(msg.sender == owner, "RDT:UVS:NOT_OWNER");
-        require(totalSupply != 0,    "RDT:UVS:ZERO_SUPPLY");
+        require(totalSupply != 0, "RDT:UVS:ZERO_SUPPLY");
 
         // Update "y-intercept" to reflect current available asset.
         freeAssets_ = freeAssets = totalClaimableRevenueAssets();
 
         // Calculate slope.
-        issuanceRate_ = issuanceRate = ((ERC20(revenueAsset).balanceOf(address(this)) - freeAssets_) * precision) / vestingPeriod_;
+        issuanceRate_ = issuanceRate =
+            ((ERC20(revenueAsset).balanceOf(address(this)) - freeAssets_) * precision) /
+            vestingPeriod_;
 
         // Update timestamp and period finish.
         vestingPeriodFinish = (lastUpdated = block.timestamp) + vestingPeriod_;
@@ -98,7 +116,13 @@ contract RevenueDistributionToken is IRevenueDistributionToken, ERC20 {
     /*** Staker Functions ***/
     /************************/
 
-    function deposit(uint256 asset_, address receiver_) external virtual override nonReentrant returns (uint256 shares_) {
+    function deposit(uint256 asset_, address receiver_)
+        external
+        virtual
+        override
+        nonReentrant
+        returns (uint256 shares_)
+    {
         _mint(shares_ = asset_, asset_, receiver_, msg.sender);
     }
 
@@ -106,12 +130,10 @@ contract RevenueDistributionToken is IRevenueDistributionToken, ERC20 {
         uint256 asset_,
         address receiver_,
         uint256 deadline_,
-        uint8   v_,
+        uint8 v_,
         bytes32 r_,
         bytes32 s_
-    )
-        external virtual override nonReentrant returns (uint256 shares_)
-    {
+    ) external virtual override nonReentrant returns (uint256 shares_) {
         ERC20(asset).permit(msg.sender, address(this), asset_, deadline_, v_, r_, s_);
         _mint(shares_ = asset_, asset_, receiver_, msg.sender);
     }
@@ -125,38 +147,64 @@ contract RevenueDistributionToken is IRevenueDistributionToken, ERC20 {
         address receiver_,
         uint256 maxAssets_,
         uint256 deadline_,
-        uint8   v_,
+        uint8 v_,
         bytes32 r_,
         bytes32 s_
-    )
-        external virtual override nonReentrant returns (uint256 assets_)
-    {
+    ) external virtual override nonReentrant returns (uint256 assets_) {
         require((assets_ = previewMint(shares_)) <= maxAssets_, "RDT:MWP:INSUFFICIENT_PERMIT");
 
         ERC20(asset).permit(msg.sender, address(this), maxAssets_, deadline_, v_, r_, s_);
         _mint(shares_, assets_, receiver_, msg.sender);
     }
 
-    function withdraw(uint256 asset_, address receiver_, address owner_) external virtual override nonReentrant returns (uint256 shares_) {
+    function withdraw(
+        uint256 asset_,
+        address receiver_,
+        address owner_
+    ) external virtual override nonReentrant returns (uint256 shares_) {
         _burn(shares_ = asset_, asset_, receiver_, owner_, msg.sender);
     }
-    
-    function redeem(uint256 shares_, address receiver_, address owner_) external override returns (uint256 assets_) {
+
+    function redeem(
+        uint256 shares_,
+        address receiver_,
+        address owner_
+    ) external override returns (uint256 assets_) {
         _burn(shares_, assets_ = shares_, receiver_, owner_, msg.sender);
     }
 
     function claim(address receiver_) external virtual nonReentrant returns (uint256 claimableRevenueAssets_) {
         require(ERC20Helper.transfer(revenueAsset, receiver_, claimableRevenueAssets(receiver_)), "RDT:C:TRANSFER");
+
+        claimableRevenueAssets_ = claimableRevenueAssets(receiver_);
+    }
+
+    function migrate(
+        address receiver_,
+        address xTRI_,
+        uint256 xTRIAmount_
+    ) external virtual nonReentrant returns (uint256 shares_) {
+        require(ERC20(xTRI_).transferFrom(receiver_, address(this), xTRIAmount_));
+        uint256 triBalanceBefore = ERC20(asset).balanceOf(address(this));
+        ITriBar(xTRI_).leave(xTRIAmount_);
+        uint256 triBalanceUnstaked = ERC20(asset).balanceOf(address(this)) - triBalanceBefore;
+
+        _mint(shares_ = triBalanceUnstaked, triBalanceUnstaked, receiver_, address(this));
     }
 
     /**************************/
     /*** Internal Functions ***/
     /**************************/
 
-    function _mint(uint256 shares_, uint256 triAmount_, address receiver_, address caller_) internal {
-        require(receiver_    != address(0), "RDT:M:ZERO_RECEIVER");
-        require(shares_      != uint256(0), "RDT:M:ZERO_SHARES");
-        require(triAmount_   != uint256(0), "RDT:M:ZERO_ASSETS");
+    function _mint(
+        uint256 shares_,
+        uint256 triAmount_,
+        address receiver_,
+        address caller_
+    ) internal {
+        require(receiver_ != address(0), "RDT:M:ZERO_RECEIVER");
+        require(shares_ != uint256(0), "RDT:M:ZERO_SHARES");
+        require(triAmount_ != uint256(0), "RDT:M:ZERO_ASSETS");
 
         _mint(receiver_, shares_);
 
@@ -170,15 +218,21 @@ contract RevenueDistributionToken is IRevenueDistributionToken, ERC20 {
         require(ERC20Helper.transferFrom(asset, caller_, address(this), triAmount_), "RDT:M:TRANSFER_FROM");
     }
 
-    function _burn(uint256 shares_, uint256 triAmount_, address receiver_, address owner_, address caller_) internal {
-        require(receiver_   != address(0), "RDT:B:ZERO_RECEIVER");
-        require(shares_     != uint256(0), "RDT:B:ZERO_SHARES");
-        require(triAmount_  != uint256(0), "RDT:B:ZERO_ASSETS");
+    function _burn(
+        uint256 shares_,
+        uint256 triAmount_,
+        address receiver_,
+        address owner_,
+        address caller_
+    ) internal {
+        require(receiver_ != address(0), "RDT:B:ZERO_RECEIVER");
+        require(shares_ != uint256(0), "RDT:B:ZERO_SHARES");
+        require(triAmount_ != uint256(0), "RDT:B:ZERO_ASSETS");
         require(balanceOf[owner_] >= shares_, "RDT:B:INSUFFICIENT_BALANCE");
 
         if (caller_ != owner_) {
             _decreaseAllowance(owner_, caller_, shares_);
-        }        
+        }
 
         _burn(owner_, shares_);
 
@@ -204,29 +258,37 @@ contract RevenueDistributionToken is IRevenueDistributionToken, ERC20 {
         return convertSharesToClaimableRevenueAssets(balanceOf[account_]);
     }
 
-    function convertSharesToClaimableRevenueAssets(uint256 shares_) public view virtual returns (uint256 claimableRevenueAssets_) {
-        uint256 supply = totalSupply;  // Cache to stack.
+    function convertSharesToClaimableRevenueAssets(uint256 shares_)
+        public
+        view
+        virtual
+        returns (uint256 claimableRevenueAssets_)
+    {
+        uint256 supply = totalSupply; // Cache to stack.
 
-
-        claimableRevenueAssets_ = (supply == 0 || shares_ == 0) ? 0 : (shares_ * totalClaimableRevenueAssets()) / supply;
+        claimableRevenueAssets_ = (supply == 0 || shares_ == 0)
+            ? 0
+            : (shares_ * totalClaimableRevenueAssets()) / supply;
     }
 
     function convertToShares(uint256 assets_) public view virtual override returns (uint256 shares_) {
-        return assets_;
+        uint256 supply = totalSupply; // Cache to stack.
+
+        shares_ = supply == 0 ? assets_ : (assets_ * supply) / totalClaimableRevenueAssets();
     }
 
     function maxDeposit(address receiver_) external pure virtual override returns (uint256 maxAssets_) {
-        receiver_;  // Silence warning
+        receiver_; // Silence warning
         maxAssets_ = type(uint256).max;
     }
 
     function maxMint(address receiver_) external pure virtual override returns (uint256 maxShares_) {
-        receiver_;  // Silence warning
+        receiver_; // Silence warning
         maxShares_ = type(uint256).max;
     }
-    
+
     function maxRedeem(address owner_) external pure virtual override returns (uint256 maxShares_) {
-        owner_;  // Silence warning
+        owner_; // Silence warning
         maxShares_ = type(uint256).max;
     }
 
@@ -245,7 +307,7 @@ contract RevenueDistributionToken is IRevenueDistributionToken, ERC20 {
     function previewWithdraw(uint256 assets_) public pure virtual override returns (uint256 shares_) {
         return assets_;
     }
-    
+
     function previewRedeem(uint256 shares_) public pure virtual override returns (uint256 assets_) {
         return shares_;
     }
@@ -253,7 +315,7 @@ contract RevenueDistributionToken is IRevenueDistributionToken, ERC20 {
     function totalAssets() external view override returns (uint256 totalAssets_) {
         return ERC20(asset).balanceOf(address(this));
     }
-    
+
     function totalRevenueAssets() external view returns (uint256 totalAssets_) {
         return ERC20(revenueAsset).balanceOf(address(this));
     }
@@ -265,12 +327,11 @@ contract RevenueDistributionToken is IRevenueDistributionToken, ERC20 {
         if (issuanceRate_ == 0) return freeAssets;
 
         uint256 vestingPeriodFinish_ = vestingPeriodFinish;
-        uint256 lastUpdated_         = lastUpdated;
+        uint256 lastUpdated_ = lastUpdated;
 
-        uint256 vestingTimePassed =
-            block.timestamp > vestingPeriodFinish_ ?
-                vestingPeriodFinish_ - lastUpdated_ :
-                block.timestamp - lastUpdated_;
+        uint256 vestingTimePassed = block.timestamp > vestingPeriodFinish_
+            ? vestingPeriodFinish_ - lastUpdated_
+            : block.timestamp - lastUpdated_;
 
         return ((issuanceRate_ * vestingTimePassed) / precision) + freeAssets;
     }
@@ -288,6 +349,6 @@ contract RevenueDistributionToken is IRevenueDistributionToken, ERC20 {
     /**************************/
 
     function _divRoundUp(uint256 numerator_, uint256 divisor_) internal pure returns (uint256 result_) {
-       return (numerator_ / divisor_) + (numerator_ % divisor_ > 0 ? 1 : 0);
+        return (numerator_ / divisor_) + (numerator_ % divisor_ > 0 ? 1 : 0);
     }
 }
