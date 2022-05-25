402095858deef868f97b6679743ddafdd80dfb9b
specialist0x <specialist0x@protonmail.com>
2022-05-25 10:17:10 -0400

    Fixed hardhat watch

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import "hardhat-watcher"
 
 import "./tasks/accounts";
 import "./tasks/clean";
