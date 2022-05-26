1b22e8a950dd9b34f3747cf3caa9d58b0eda17b4
Eric Juta <ericjohnjuta@gmail.com>
2022-05-26 13:34:57 -0400

    test(test/stabletristaking/migrate.test.ts): add 2x migrate and migrate after rewardToken deposit

--- test/stableTRIStaking/migrate.test.ts
+++ test/stableTRIStaking/migrate.test.ts
@@ -2,13 +2,15 @@ import { ethers } from "hardhat";
 import chai from "chai";
 import { solidity } from "ethereum-waffle";
 import { Tri__factory } from "../../typechain";
-import { getBigNumber } from "../utils";
+import { getBigNumber, increaseTimeBySeconds } from "../utils";
 
 chai.use(solidity);
 const { expect } = chai;
 
 describe("RevenueDistributionToken - Migrate", function () {
   beforeEach(async function () {
+    await ethers.provider.send("hardhat_reset", []);
+
     const [deployer, user0] = await ethers.getSigners();
     this.deployer = deployer;
     this.user0 = user0;
@@ -22,15 +24,8 @@ describe("RevenueDistributionToken - Migrate", function () {
     this.pTRI = await ethers.getContractFactory("StableTRIStaking");
     const ERC20Mock = await ethers.getContractFactory("ERC20Mock");
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    this.rewardToken = await ERC20Mock.deploy("rewardToken", "rewardToken", 18, getBigNumber("300") as any);
-    const pTRIConstructorArgs = [
-      "pTRI",
-      "pTRI",
-      this.rewardToken.address,
-      this.tri.address,
-      this.deployer.address,
-      0,
-    ];
+    this.rewardToken = await ERC20Mock.deploy("rewardToken", "rewardToken", 18, 100 as any);
+    const pTRIConstructorArgs = ["pTRI", "pTRI", this.rewardToken.address, this.tri.address, this.deployer.address, 0];
     this.pTRI = await this.pTRI.deploy(...pTRIConstructorArgs);
     await this.xTRI.approve(this.pTRI.address, "100");
   });
@@ -42,6 +37,32 @@ describe("RevenueDistributionToken - Migrate", function () {
     expect(await this.xTRI.balanceOf(this.deployer.address)).to.equal("0");
     expect(await this.tri.balanceOf(this.pTRI.address)).to.equal("100");
     expect(await this.pTRI.balanceOf(this.deployer.address)).to.equal("100");
+  });
+
+  it("should migrate, migrate again and claim initial", async function () {
+    await this.rewardToken.transfer(this.pTRI.address, "100");
+    await this.tri.mint(this.deployer.address, "100");
+    await this.tri.approve(this.xTRI.address, "100");
+    await this.xTRI.enter("100");
+
+    expect(await this.xTRI.balanceOf(this.deployer.address)).to.equal("200");
+
+    await this.pTRI.connect(this.deployer).migrate(this.xTRI.address, "100");
+
+    expect(await this.tri.balanceOf(this.deployer.address)).to.equal("0");
+    expect(await this.xTRI.balanceOf(this.deployer.address)).to.equal("100");
+
+    expect(await this.tri.balanceOf(this.pTRI.address)).to.equal("100");
+    expect(await this.pTRI.balanceOf(this.deployer.address)).to.equal("100");
+
+    await this.xTRI.approve(this.pTRI.address, "100");
+    await this.pTRI.connect(this.deployer).migrate(this.xTRI.address, "100");
+
+    expect(await this.tri.balanceOf(this.pTRI.address)).to.equal("200");
+    expect(await this.pTRI.balanceOf(this.deployer.address)).to.equal("200");
+
+    expect(await this.rewardToken.balanceOf(this.deployer.address)).to.equal("100");
+    expect(await this.rewardToken.balanceOf(this.pTRI.address)).to.equal("0");
   });
 
   it("should fail migrate if user has no xTRI", async function () {
@@ -61,20 +82,16 @@ describe("RevenueDistributionToken - Migrate", function () {
     await this.pTRI.connect(this.user0).migrate(this.xTRI.address, "100");
     expect(await this.pTRI.balanceOf(this.user0.address)).to.equal("100");
   });
-  
-  it("should fail migrate if user has no xTRI", async function () {
-    await this.rewardToken.transfer(this.pTRI.address, ethers.utils.parseEther("4"));
-    await increase(86400);
+
+  it("should migrate but claim zero if user has not migrated before", async function () {
+    await this.rewardToken.transfer(this.pTRI.address, "100");
+    await increaseTimeBySeconds(86400);
 
     expect(await this.xTRI.balanceOf(this.deployer.address)).to.equal("100");
     await this.pTRI.connect(this.deployer).migrate(this.xTRI.address, "100");
 
+    expect(await this.rewardToken.balanceOf(this.deployer.address)).to.equal("0");
     expect(await this.pTRI.balanceOf(this.deployer.address)).to.equal("100");
     expect(await this.tri.balanceOf(this.deployer.address)).to.equal("0");
   });
 });
-
-const increase = (seconds: number) => {
-  void ethers.provider.send("evm_increaseTime", [seconds]);
-  void ethers.provider.send("evm_mine", []);
-};
