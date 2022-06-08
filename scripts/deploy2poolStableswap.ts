09471b0cf1b1109ddd6b4a2d75b50a43f0204d11
Eric Juta <ericjohnjuta@gmail.com>
2022-06-08 15:32:07 +0100

    feat(scripts/deploy2poolstableswap.ts): usdc-usdt pool

---
+++ scripts/deploy2poolStableswap.ts
@@ -0,0 +1,114 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import {
+  ethers,
+  // , run
+} from "hardhat";
+import { usdcAddress, usdtAddress } from "./constants";
+
+async function main(): Promise<void> {
+  // Hardhat always runs the compile task when running scripts through it.
+  // If this runs in a standalone fashion you may want to call compile manually
+  // to make sure everything is compiled
+  // await run("compile");
+  // We get the contract to deploy
+
+  // Constants
+  const [_, deployer] = await ethers.getSigners();
+  console.log(`Deploying contracts with ${deployer.address}`);
+
+  const erc20Factory = await ethers.getContractFactory("ERC20Mock");
+  const usdc = erc20Factory.attach(usdcAddress);
+  const usdt = erc20Factory.attach(usdtAddress);
+
+  const usdcDecimals = await usdc.decimals();
+  const usdtDecimals = await usdt.decimals();
+
+  const LpTokenFactory = await ethers.getContractFactory("LPToken", deployer);
+  const lpTokenBase = await LpTokenFactory.deploy();
+  await lpTokenBase.deployed();
+  await lpTokenBase.initialize("Test Token", "TEST");
+  console.log(`LPToken Base deployed at ${lpTokenBase.address}`);
+
+  const AmpUtilsFactory = await ethers.getContractFactory("AmplificationUtils", deployer);
+  const amplificationUtils = await AmpUtilsFactory.deploy();
+  await amplificationUtils.deployed();
+  console.log(`amplificationUtils deployed at ${amplificationUtils.address}`);
+
+  const SwapUtilsFactory = await ethers.getContractFactory("SwapUtils", deployer);
+  const swapUtils = await SwapUtilsFactory.deploy();
+  await swapUtils.deployed();
+  console.log(`swapUtils deployed at ${swapUtils.address}`);
+
+  const SwapFlashLoanFactory = await ethers.getContractFactory("SwapFlashLoan", {
+    libraries: {
+      SwapUtils: swapUtils.address,
+      AmplificationUtils: amplificationUtils.address,
+    },
+  });
+  const swapFlashLoan = await SwapFlashLoanFactory.connect(deployer).deploy();
+  await swapFlashLoan.deployed();
+  console.log(`swapFlashLoan deployed at ${swapFlashLoan.address}`);
+
+  // Constructor arguments
+  const TOKEN_ADDRESSES = [usdc.address, usdt.address];
+  const TOKEN_DECIMALS = [usdcDecimals, usdtDecimals];
+  const LP_TOKEN_NAME = "Trisolaris USDC/USDT";
+  const LP_TOKEN_SYMBOL = "USD TLP";
+  const INITIAL_A = 1000;
+  const SWAP_FEE = 4e6; // 4bps
+  const ADMIN_FEE = 10 * 10e8; // 100 %
+
+  await swapFlashLoan
+    .connect(deployer)
+    .initialize(
+      TOKEN_ADDRESSES,
+      TOKEN_DECIMALS,
+      LP_TOKEN_NAME,
+      LP_TOKEN_SYMBOL,
+      INITIAL_A,
+      SWAP_FEE,
+      ADMIN_FEE,
+      lpTokenBase.address,
+    );
+  const swapStorage = await swapFlashLoan.swapStorage();
+  const lpToken = LpTokenFactory.attach(swapStorage.lpToken);
+  console.log(`lpToken deployed at ${lpToken.address}`);
+
+  // Verify contracts
+  // await run("verify:verify", {
+  //   address: "0xB77190A4fD2528d2Bb778B409FB5224f7ffaCB24",
+  //   constructorArguments: [],
+  // });
+
+  // await run("verify:verify", {
+  //   address: "0x114ECaa70256aFAd393f733aA4B4bF61c8959fc2",
+  //   constructorArguments: [],
+  // });
+
+  // await run("verify:verify", {
+  //   address: "0x0564d68404608599e8c567A0bD74F90a942A69A0",
+  //   constructorArguments: [],
+  // });
+
+  // await run("verify:verify", {
+  //   address: "0x458459E48dbAC0C8Ca83F8D0b7b29FEfE60c3970",
+  //   constructorArguments: [],
+  // });
+
+  // await run("verify:verify", {
+  //   address: "0x87BCC091d0A7F9352728100268Ac8D25729113bB",
+  //   constructorArguments: [],
+  // });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+  });
