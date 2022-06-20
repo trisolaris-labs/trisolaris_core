b9e8b230c028b06137fb7d966d6ed4ae9d88f320
Eric Juta <ericjohnjuta@gmail.com>
2022-06-20 17:00:15 +0100

    feat(scripts/ops/pointstablepoolstostablelpmakerv2.ts): allow stablelpmakerv2 to withdraw fees

---
+++ scripts/ops/pointStablePoolsToStableLPMakerV2.ts
@@ -0,0 +1,50 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+import {
+  nusdPoolSwapDepositAddress,
+  nusdPoolSwapFlashLoanAddress,
+  stableLPMakerV2Address,
+  threePoolSwapFlashLoanAddress,
+  twoPoolSwapFlashLoanAddress,
+} from "../constants";
+
+async function main(): Promise<void> {
+  // Hardhat always runs the compile task when running scripts through it.
+  // If this runs in a standalone fashion you may want to call compile manually
+  // to make sure everything is compiled
+  // await run("compile");
+  // We get the contract to deploy
+
+  const [_, deployer] = await ethers.getSigners();
+  console.log(`Calling contracts with ${deployer.address}`);
+
+  const balance = await deployer.getBalance();
+  console.log(`Account balance: ${balance.toString()}`);
+
+  const stableLPMakerV2 = { address: stableLPMakerV2Address };
+
+  const stableSwapPool = await ethers.getContractFactory("SwapFlashLoan");
+
+  await stableSwapPool.connect(deployer).attach(threePoolSwapFlashLoanAddress).setFeeAddress(stableLPMakerV2.address);
+  console.log(
+    `stableSwapPool.connect(deployer).attach(${threePoolSwapFlashLoanAddress}).setFeeAddress(${stableLPMakerV2.address})`,
+  );
+
+  await stableSwapPool.connect(deployer).attach(twoPoolSwapFlashLoanAddress).setFeeAddress(stableLPMakerV2.address);
+  console.log(
+    `stableSwapPool.connect(deployer).attach(${twoPoolSwapFlashLoanAddress}).setFeeAddress(${stableLPMakerV2.address})`,
+  );
+
+  await stableSwapPool.connect(deployer).attach(nusdPoolSwapFlashLoanAddress).setFeeAddress(stableLPMakerV2.address);
+  console.log(
+    `stableSwapPool.connect(deployer).attach(${nusdPoolSwapDepositAddress}).setFeeAddress(${stableLPMakerV2.address})`,
+  );
+}
+
+main().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
