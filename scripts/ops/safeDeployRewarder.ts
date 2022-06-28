789b4c29d0d68c2f8ba961052f16d466de6de2e1
Eric Juta <ericjohnjuta@gmail.com>
2022-06-28 13:50:08 +0100

    feat(safedeployrewarder.ts): deploy new rewarder, transfer ownership, propose add to mcv2

--- scripts/ops/safeDeployRewarder.ts
+++ scripts/ops/safeDeployRewarder.ts
@@ -5,12 +5,28 @@ import EthersAdapter from "@gnosis.pm/safe-ethers-lib";
 import { SafeEthersSigner, SafeService } from "@gnosis.pm/safe-ethers-adapters";
 import { Wallet } from "ethers";
 import { JsonRpcProvider } from "@ethersproject/providers";
-import { auroraURL, chefV2Address, safeServiceURL } from "../constants";
+import { auroraURL, chefV2Address, dao, safeServiceURL, triAddress } from "../constants";
+import { ComplexRewarder } from "../../typechain";
 
 type RewarderConfig = {
   lpToken: string;
   rewardToken: string;
 };
+type DeployedRewarder = {
+  rewarder: ComplexRewarder;
+};
+
+const provider = new JsonRpcProvider(auroraURL);
+const { safeAddress = undefined, safeSignerPK = undefined } = process.env;
+if (!safeSignerPK) {
+  throw new Error("*** SAFE SIGNER PK NOT FOUND IN ENV ***");
+}
+if (!safeAddress) {
+  throw new Error("*** SAFE ADDRESS NOT FOUND IN ENV ***");
+}
+
+const deployer = new Wallet(safeSignerPK).connect(provider);
+
 const addNewRewarderConfigToExistingJSON = async (newRewarderConfig: RewarderConfig) => {
   try {
     const rewarderConfigsJSONFile = await fs.readFile("./rewarderConfigs.json");
@@ -28,35 +44,68 @@ const addNewRewarderConfigToExistingJSON = async (newRewarderConfig: RewarderCon
   }
 };
 
-const _proposeAddingNewRewarderToSafe = async (newRewarderConfig: RewarderConfig) => {
-  const { safeAddress = undefined, safeSignerPK = undefined } = process.env;
-  if (!safeSignerPK) {
-    console.error(new Error("*** SAFE SIGNER PK NOT FOUND IN ENV ***"));
-    return;
-  }
-  if (!safeAddress) {
-    console.error(new Error("*** SAFE ADDRESS NOT FOUND IN ENV ***"));
-    return;
-  }
+const transferRewarderOwnershipToDAO = async ({ rewarder }: DeployedRewarder) => {
+  const complexRewarder = await ethers.getContractFactory("ComplexRewarder");
+  const tx = await complexRewarder.connect(deployer).attach(rewarder.address).transferOwnership(dao);
+  await tx.wait();
 
-  const { rewardToken, lpToken } = newRewarderConfig;
-  const tokenPerBlock = "0";
+  console.info("*** Transferred Rewarder at: ", rewarder.address, " ownership to DAO: ", dao);
+};
 
-  const provider = new JsonRpcProvider(auroraURL);
+// TODO:
+// Do we need this as a proposed safe tx? Migrating MCV2 to DAO?
+const proposeAddPoolChefV2 = async (
+  { rewarder }: DeployedRewarder,
+  newRewarderConfig: RewarderConfig,
+): Promise<void> => {
+  // Config
+  const { lpToken } = newRewarderConfig;
+  const allocPoint = 0;
+
   const service = new SafeService(safeServiceURL);
-  const signer = new Wallet(safeSignerPK, provider);
+  const signer = deployer;
   console.log("Setup SafeEthersSigner");
-
   const ethAdapter = new EthersAdapter({ ethers, signer });
   const safe = await Safe.create({ ethAdapter, safeAddress });
   const safeSigner = new SafeEthersSigner(safe, service, provider);
 
+  const masterChefV2 = await ethers.getContractFactory("MasterChefV2");
+  const triToken = await ethers.getContractFactory("Tri");
+  const tri = triToken.attach(triAddress);
+  console.info(`Tri address: ${tri.address}`);
+  const chef = masterChefV2.attach(chefV2Address);
+  console.info(`Chef address: ${chef.address}`);
+
+  const poolLength = await chef.poolLength();
+  let canAddPool = true;
+  for (let i = 0; i < poolLength.toNumber(); i++) {
+    const lpTokenAddress = await chef.lpToken(i);
+    if (lpTokenAddress === lpToken) {
+      canAddPool = false;
+    }
+  }
+  if (canAddPool) {
+    console.info("*** Propose adding new pool to MCV2:", lpToken);
+    const tx = await chef.connect(safeSigner).add(allocPoint, lpToken, rewarder.address);
+    await tx.wait();
+
+    console.log("*** USER ACTION REQUIRED ***");
+    console.log("Go to the Gnosis Safe Web App to confirm the transaction");
+    console.log(`*** Please verify the proposed adding pool to MCV2 after at: ${rewarder.address}`);
+  } else {
+    throw new Error(`*** lpToken address already added in MCV2 Pool: ${lpToken}`);
+  }
+};
+
+const deployNewRewarder = async (newRewarderConfig: RewarderConfig): Promise<DeployedRewarder> => {
+  const { rewardToken, lpToken } = newRewarderConfig;
+  const tokenPerBlock = "0";
+
   const complexRewarder = await ethers.getContractFactory("ComplexRewarder");
-  const rewarder = await complexRewarder.connect(safeSigner).deploy(rewardToken, lpToken, tokenPerBlock, chefV2Address);
+  const rewarder = await complexRewarder.connect(deployer).deploy(rewardToken, lpToken, tokenPerBlock, chefV2Address);
   await rewarder.deployed();
-  console.log("*** USER ACTION REQUIRED ***");
-  console.log("Go to the Gnosis Safe Web App to confirm the transaction");
-  console.log(`*** Please verify the new rewarder contract post confirmation after at: ${rewarder.address}`);
+
+  return { rewarder };
 };
 
 async function main() {
@@ -67,12 +116,11 @@ async function main() {
     try {
       const newRewarderConfigJSONFile = await fs.readFile("./newRewarderConfig.json");
       const newRewarderConfig = JSON.parse(newRewarderConfigJSONFile?.toString());
-      console.log(newRewarderConfig);
 
-      // TODO: 0xchain to add combined scripts here
-      // Add Rewarder to MCV2 etc.
-      // await proposeAddingNewRewarderToSafe(newRewarderConfig);
-      //
+      // TODO: 0xchain to verify whether this is correct process?
+      const rewarder = await deployNewRewarder(newRewarderConfig);
+      await transferRewarderOwnershipToDAO(rewarder);
+      await proposeAddPoolChefV2(rewarder, newRewarderConfig);
 
       await addNewRewarderConfigToExistingJSON(newRewarderConfig);
     } catch (err) {
