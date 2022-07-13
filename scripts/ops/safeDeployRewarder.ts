f4dfccc1c126bcbdcf87d396f89bca7c83536cf0
Eric Juta <ericjohnjuta@gmail.com>
2022-07-13 19:10:47 +0100

    feat(scripts/ops/safedeployrewarder.ts): if no reward token, do not deploy rewarder contract
    no reward contract address to add to rewarder config

--- scripts/ops/safeDeployRewarder.ts
+++ scripts/ops/safeDeployRewarder.ts
@@ -5,13 +5,13 @@ import EthersAdapter from "@gnosis.pm/safe-ethers-lib";
 import { SafeEthersSigner, SafeService } from "@gnosis.pm/safe-ethers-adapters";
 import { Wallet } from "ethers";
 import { JsonRpcProvider } from "@ethersproject/providers";
-import { chefV2Address, ops, triAddress } from "../constants";
+import { chefV2Address, ops, triAddress, zeroAddress } from "../constants";
 import { ComplexRewarder } from "../../typechain";
 
 type RewarderConfig = {
   LPToken: string;
-  RewardToken: string;
-  Rewarder: string;
+  RewardToken?: string;
+  Rewarder?: string;
   PoolId: number;
   RewardTokenDecimals: number;
   CoingeckoRewarderTokenName?: string;
@@ -42,37 +42,63 @@ console.info("*** Using SAFE_SERVICE_URL: ", SAFE_SERVICE_URL);
 
 const addNewRewarderConfigToExistingJSON = async (
   PoolId: number,
-  { rewarder }: DeployedRewarder,
+  deployedRewarder: DeployedRewarder | undefined,
   RewardTokenDecimals: number,
   newRewarderConfig: RewarderConfig,
 ) => {
-  const rewarderConfigsJSON: RewarderConfig[] = await fs.readJSON("./rewarderConfigs.json");
+  if (deployedRewarder) {
+    const { rewarder } = deployedRewarder;
+    const rewarderConfigsJSON: RewarderConfig[] = await fs.readJSON("./rewarderConfigs.json");
 
-  const rewarderConfig: RewarderConfig = {
-    ...newRewarderConfig,
-    Rewarder: rewarder.address,
-    PoolId: PoolId,
-    RewardTokenDecimals,
-  };
-  rewarderConfigsJSON.push(rewarderConfig);
+    const rewarderConfig: RewarderConfig = {
+      ...newRewarderConfig,
+      Rewarder: rewarder.address,
+      PoolId: PoolId,
+      RewardTokenDecimals,
+    };
+    rewarderConfigsJSON.push(rewarderConfig);
 
-  await fs.writeJSON("./rewarderConfigs.json", rewarderConfigsJSON);
-  console.info("*** Added new rewarder config to rewarderConfigs.json");
+    await fs.writeJSON("./rewarderConfigs.json", rewarderConfigsJSON);
+    console.info("*** Added new rewarder config to rewarderConfigs.json");
 
-  // NOTE - Used because fs.promises.rm is not a function error on github actions, weird
-  await fs.remove("./newRewarderConfig.json");
-  console.info("*** Removed newRewarderConfig.json file, no longer needed");
+    // NOTE - Used because fs.promises.rm is not a function error on github actions, weird
+    await fs.remove("./newRewarderConfig.json");
+    console.info("*** Removed newRewarderConfig.json file, no longer needed");
+  } else {
+    const rewarderConfigsJSON: RewarderConfig[] = await fs.readJSON("./rewarderConfigs.json");
+
+    const rewarderConfig: RewarderConfig = {
+      ...newRewarderConfig,
+      PoolId: PoolId,
+      RewardTokenDecimals,
+    };
+    rewarderConfigsJSON.push(rewarderConfig);
+
+    await fs.writeJSON("./rewarderConfigs.json", rewarderConfigsJSON);
+    console.info(
+      "*** Added new rewarder config to rewarderConfigs.json - (Did not deploy new rewarder contract as no RewardToken)",
+    );
+
+    // NOTE - Used because fs.promises.rm is not a function error on github actions, weird
+    await fs.remove("./newRewarderConfig.json");
+    console.info("*** Removed newRewarderConfig.json file, no longer needed");
+  }
 };
 
 const getRewarderERC20Decimals = async (
   newRewarderConfig: RewarderConfig,
 ): Promise<{ RewardTokenDecimals: number }> => {
   const rewardToken = await ethers.getContractFactory("ERC20");
-  const RewardTokenDecimals = await rewardToken.connect(deployer).attach(newRewarderConfig.RewardToken).decimals();
+  if (newRewarderConfig?.RewardToken) {
+    const RewardTokenDecimals = await rewardToken.connect(deployer).attach(newRewarderConfig.RewardToken).decimals();
 
-  console.info("*** RewardTokenDecimals found as: ", RewardTokenDecimals);
+    console.info("*** RewardTokenDecimals found as: ", RewardTokenDecimals);
 
-  return { RewardTokenDecimals };
+    return { RewardTokenDecimals };
+  } else {
+    // NOTE - $TRI is the reward token decimals used here so we default to 18
+    return { RewardTokenDecimals: 18 };
+  }
 };
 
 const transferRewarderOwnershipToDAO = async ({ rewarder }: DeployedRewarder) => {
@@ -84,7 +110,7 @@ const transferRewarderOwnershipToDAO = async ({ rewarder }: DeployedRewarder) =>
 };
 
 const proposeAddPoolChefV2 = async (
-  { rewarder }: DeployedRewarder,
+  deployedRewarder: DeployedRewarder | undefined,
   newRewarderConfig: RewarderConfig,
 ): Promise<{ PoolId: number }> => {
   // Config
@@ -117,14 +143,23 @@ const proposeAddPoolChefV2 = async (
   }
   if (canAddPool) {
     console.info("*** Propose adding new pool to MCV2:", LPToken);
-    const chefAddArgs = [allocPoint, LPToken, rewarder.address];
+
+    let chefAddArgs;
+    if (deployedRewarder) {
+      const { rewarder } = deployedRewarder;
+      chefAddArgs = [allocPoint, LPToken, rewarder.address];
+    }
+    //  NOTE - No deployed rewarder address because no additional reward token to distribute
+    else {
+      chefAddArgs = [allocPoint, LPToken, zeroAddress];
+    }
     console.info(JSON.stringify(chefAddArgs));
 
     await chef.connect(safeSigner).add(chefAddArgs[0], chefAddArgs[1]?.toString(), chefAddArgs[2]?.toString());
 
     console.info("*** USER ACTION REQUIRED ***");
     console.info("Go to the Gnosis Safe Web App to confirm the transaction");
-    console.info(`*** Please verify the proposed adding pool to MCV2 after at: ${rewarder.address}`);
+    console.info(`*** Please verify the proposed adding pool to MCV2`);
 
     return { PoolId };
   } else {
@@ -134,6 +169,11 @@ const proposeAddPoolChefV2 = async (
 
 const deployNewRewarder = async (newRewarderConfig: RewarderConfig): Promise<DeployedRewarder> => {
   const { RewardToken, LPToken } = newRewarderConfig;
+  if (!RewardToken) {
+    throw new Error(
+      "*** No Reward Token found, attempted to deploy new rewarder without one, should have never reached here!",
+    );
+  }
   const tokenPerBlock = "0";
 
   console.info(
@@ -170,7 +210,7 @@ const deployNewRewarder = async (newRewarderConfig: RewarderConfig): Promise<Dep
 async function main() {
   console.info("*** Proposing adding new rewarder ***");
 
-  let newRewarderConfig;
+  let newRewarderConfig: RewarderConfig | undefined;
   try {
     newRewarderConfig = await fs.readJSON("./newRewarderConfig.json");
     console.info("*** newRewarderConfig.json found ***");
@@ -180,12 +220,21 @@ async function main() {
   }
 
   if (newRewarderConfig) {
-    const rewarder = await deployNewRewarder(newRewarderConfig);
-    await transferRewarderOwnershipToDAO(rewarder);
-    const { PoolId } = await proposeAddPoolChefV2(rewarder, newRewarderConfig);
+    if (typeof newRewarderConfig?.RewardToken === "string") {
+      const rewarder = await deployNewRewarder(newRewarderConfig);
+      await transferRewarderOwnershipToDAO(rewarder);
+      const { PoolId } = await proposeAddPoolChefV2(rewarder, newRewarderConfig);
 
-    const { RewardTokenDecimals } = await getRewarderERC20Decimals(newRewarderConfig);
-    await addNewRewarderConfigToExistingJSON(PoolId, rewarder, RewardTokenDecimals, newRewarderConfig);
+      const { RewardTokenDecimals } = await getRewarderERC20Decimals(newRewarderConfig);
+      await addNewRewarderConfigToExistingJSON(PoolId, rewarder, RewardTokenDecimals, newRewarderConfig);
+    }
+    // NOTE - No rewarder to deploy because no reward token so we add to MCV2 with zero address for the rewarder param
+    else {
+      const { PoolId } = await proposeAddPoolChefV2(undefined, newRewarderConfig);
+
+      const { RewardTokenDecimals } = await getRewarderERC20Decimals(newRewarderConfig);
+      await addNewRewarderConfigToExistingJSON(PoolId, undefined, RewardTokenDecimals, newRewarderConfig);
+    }
   }
 }
 
