d49db8ff6e7ce27b861acf5523603814463689f4
Baboo Bhaiya <81858327+baboobhaiya@users.noreply.github.com>
2022-07-18 16:14:53 +0100

    transfer fee to setter (#88)

---
+++ scripts/ops/setFeeToSetter.ts
@@ -0,0 +1,40 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { UniswapV2Factory__factory } from "../../typechain";
+import { ethers } from "hardhat";
+import { factoryAddress, opsMultiSigAddress } from "../constants";
+
+async function main(): Promise<void> {
+  // Hardhat always runs the compile task when running scripts through it.
+  // If this runs in a standalone fashion you may want to call compile manually
+  // to make sure everything is compiled
+  // await run("compile");
+  // We get the contract to deploy
+  const [_, deployer] = await ethers.getSigners();
+  console.log(`Deploying contracts with ${deployer.address}`);
+
+  const uniFactory: UniswapV2Factory__factory = await ethers.getContractFactory("UniswapV2Factory");
+  const factory = uniFactory.attach(factoryAddress);
+  console.log(`Factory address: ${factory.address}`);
+  let feeToSetter = await factory.feeToSetter();
+  console.log(feeToSetter);
+  console.log(`Ops multisig: ${opsMultiSigAddress}`);
+
+  const tx = await factory.connect(deployer).setFeeToSetter(opsMultiSigAddress);
+  const receipt = await tx.wait();
+  console.log(receipt.logs);
+
+  feeToSetter = await factory.feeToSetter();
+  console.log(feeToSetter);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+  });
