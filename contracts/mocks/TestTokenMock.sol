d8030365675f1399e3c745569acfa8b51e71ffca
Diego Figueroa <diego.figueroa@aurora.dev>
2025-02-19 15:00:33 -0400

    style: lint

--- contracts/mocks/TestTokenMock.sol
+++ contracts/mocks/TestTokenMock.sol
@@ -5,7 +5,12 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestTokenMock is ERC20 {
-    constructor(string memory name, string memory symbol, uint8 decimals, uint256 supply) public ERC20(name, symbol) {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals,
+        uint256 supply
+    ) public ERC20(name, symbol) {
         _setupDecimals(decimals);
         _mint(msg.sender, supply);
     }
